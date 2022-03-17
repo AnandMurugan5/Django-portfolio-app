c4722134113eb30054a9615bcad75737c8ace84b
Muruganandham <anandmurugan2499@gmail.com>
2022-03-17 14:10:50 +0530

    master

--- portfolio_project/portfolio_app/forms.py
+++ portfolio_project/portfolio_app/forms.py
@@ -6,8 +6,8 @@ class emailform(ModelForm):
         model = contactform
         fields = '__all__'
         Widgets = {
-            'username': TextInput(attrs={'class':'form-control','placeholder':'type your name'}),
-            'email': EmailInput(attrs={'class':'form-control','placeholder':'type your name'}),
+            'Name': TextInput(attrs={'class':'form-control','placeholder':'type your name'}),
+            'Email_ID': EmailInput(attrs={'class':'form-control','placeholder':'type your name'}),
             'Description': Textarea(attrs={'col':80,'row':50}),
 
         }
--- portfolio_project/portfolio_app/models.py
+++ portfolio_project/portfolio_app/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 # Create your models here.
 class contactform(models.Model):
-    username = models.CharField(max_length=50)
-    email = models.CharField(max_length=200)
+    Name = models.CharField(max_length=50)
+    email = models.EmailField()
     Description = models.CharField(max_length=1500)
 
     def __str__(self):
-        return self.email
+        return self.Name
     
     class Mate:
         db_name = "Contect"
