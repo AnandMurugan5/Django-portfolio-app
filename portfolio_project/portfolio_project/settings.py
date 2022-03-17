5884cd8799b67aace3eac44a429cdf75f2649fe7
Muruganandham <anandmurugan2499@gmail.com>
2022-03-17 16:17:53 +0530

    edit

--- portfolio_project/portfolio_project/settings.py
+++ portfolio_project/portfolio_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-uaph%=y2b%4f44pgux0mxpg_-v@-pv%(&s@5o7b&#s=n6_t$*+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','immuruganandham.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','https://immuruganandham.herokuapp.com/']
 
 
 # Application definition
